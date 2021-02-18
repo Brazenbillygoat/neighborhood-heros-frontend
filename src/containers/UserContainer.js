2939130257ec227124a073761e08ab05f7ffe84c
Brazenbillygoat <hyrum.butler3@gmail.com>
2021-02-18 10:44:23 -0500

    Added alt text to user images for accessibility.

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -84,7 +84,7 @@ class UserContainer extends Component {
           <div className="user-card container">
             <div className="users-container row" key={user.id} myKey={user.id}>
               <div className="col-sm-6">
-                <img src={user.profile_pic} className="user-list-profile-pic" />
+                <img src={user.profile_pic} className="user-list-profile-pic" alt="User profile pic"/>
                 <p onClick={() => this.props.showUser(user)}>{user.username}</p>
               </div>
               <div className="col-sm-6">
